cdd6d7a2b430d013ec70cb776d9f408970d15f91
yulimle <yulim516@naver.com>
2023-02-20 21:42:45 +0900

    230220 programmers javascript 삼총사 문제

--- javascript/programmers/level1/삼총사.js
+++ javascript/programmers/level1/삼총사.js
@@ -1,13 +1,11 @@
 function solution(number) {
   let answer = 0;
-  let sum = 0;
-  let mid = 0;
-  for (let i = 0; i < number.length - 2; i += 1) {
-    mid = number[i] + number[i + 1];
-    for (let j = i + 2; j < number.length; j += 1) {
-      sum = mid + number[j];
-      if (sum === 0) {
-        answer += 1;
+  for (let i = 0; i < number.length; i++) {
+    for (let j = i + 1; j < number.length; j++) {
+      for (let k = j + 1; k < number.length; k++) {
+        if (number[i] + number[j] + number[k] === 0) {
+          answer++;
+        }
       }
     }
   }
