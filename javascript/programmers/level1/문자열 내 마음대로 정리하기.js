5264f96928885e7d46ddb1331f8bfd476c381b0a
yulimle <yulim516@naver.com>
2023-03-06 22:49:09 +0900

    230306 programmers 코테 연습 js

--- javascript/programmers/level1/문자열 내 마음대로 정리하기.js
+++ javascript/programmers/level1/문자열 내 마음대로 정리하기.js
@@ -1,12 +1,11 @@
 function solution(strings, n) {
-  let answer = [];
-  let order = [];
-  for (let i = 0; i < strings.length; i += 1) {
-    order.push(strings[i][n]);
-    order.sort();
-  }
-
-  return order;
+  return strings.sort((a, b) => {
+    // 인덱스 n번째 글자를 기준으로 오름차순 정렬
+    if (a[n] > b[n]) return 1;
+    else if (a[n] < b[n]) return -1;
+    // 인덱스 n번째 글자가 같으면 사전순 정렬
+    else return a > b ? 1 : -1;
+  });
 }
 
 console.log(solution(["sun", "bed", "car"], 1));
---
+++ javascript/programmers/level2/카펫.js
@@ -0,0 +1,7 @@
+function solution(brown, yellow) {
+  const answer = [];
+  return answer;
+}
+console.log(10, 2);
+console.log(8, 1);
+console.log(24, 24);
