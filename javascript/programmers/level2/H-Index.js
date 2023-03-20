bcb48b625ed9d02fb6596db890abffb936b969cf
yulimle <yulim516@naver.com>
2023-03-20 20:54:07 +0900

    230320 programmers H-Index

---
+++ javascript/programmers/level2/H-Index.js
@@ -0,0 +1,18 @@
+function solution(citations) {
+  let answer = 0;
+  let total = 0;
+  citations.sort((a, b) => a - b);
+  while (true) {
+    for (let i = 0; i < citations.length; i++) {
+      if (answer < citations[i]) {
+        total += 1;
+      }
+    }
+    if (answer >= total) break;
+    answer += 1;
+    total = 0;
+  }
+  return answer;
+}
+console.log(solution([25, 8, 5, 3, 3]));
+console.log(solution([3, 0, 6, 1, 5]));
