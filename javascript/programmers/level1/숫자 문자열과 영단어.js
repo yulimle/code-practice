3a40e616e1d6d6167f4f7644cd375ac825a3a3bf
yulimle <yulim516@naver.com>
2023-03-25 18:19:44 +0900

    230325 숫자 문자열과 영단어

--- javascript/programmers/level1/숫자 문자열과 영단어.js
+++ javascript/programmers/level1/숫자 문자열과 영단어.js
@@ -1,4 +1,25 @@
-function solution(s) {}
+function solution(s) {
+  let numbers = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  let answer = s;
+
+  for (let i = 0; i < numbers.length; i++) {
+    let arr = answer.split(numbers[i]);
+    answer = arr.join(i);
+  }
+
+  return Number(answer);
+}
 
 console.log(solution("one4seveneight"));
 console.log(solution("23four5six7"));
--- javascript/programmers/level2/비밀지도.js
+++ javascript/programmers/level2/비밀지도.js
@@ -1,21 +1,24 @@
-function solution(n, arr1, arr2) {
-  let answer = [];
+// function solution(n, arr1, arr2) {
+//   let answer = [];
 
-  for (let i = 0; i < n; i++) {
-    // toString(2)은 이진수로 반환하는 것.
-    const bin = (arr1[i] | arr2[i]).toString(2);
-    // console.log(bin);
-    let line = [];
-    for (let j = bin.length - n; j < bin.length; j++) {
-      // j가(인덱스값)이 -1일 경우 undefined 반환.
-      if (bin[j] === "1") {
-        // 1이면 '#', 그 외 ' '
-        line.push("#");
-      } else {
-        line.push(" ");
-      }
-    }
-    answer.push(line.join(""));
-  }
-  return answer;
-}
+//   for (let i = 0; i < n; i++) {
+//     // toString(2)은 이진수로 반환하는 것.
+//     const bin = (arr1[i] | arr2[i]).toString(2);
+//     // console.log(bin);
+//     let line = [];
+//     for (let j = bin.length - n; j < bin.length; j++) {
+//       // j가(인덱스값)이 -1일 경우 undefined 반환.
+//       if (bin[j] === "1") {
+//         // 1이면 '#', 그 외 ' '
+//         line.push("#");
+//       } else {
+//         line.push(" ");
+//       }
+//     }
+//     answer.push(line.join(""));
+//   }
+//   return answer;
+// }
+
+console.log(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]));
+console.log(solution(6, [46, 33, 33, 22, 31, 50], [27, 56, 19, 14, 14, 10]));
