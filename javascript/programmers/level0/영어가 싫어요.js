452d260da096d84b1d73e5d9075a8ef14a006ec4
yulimle <yulim516@naver.com>
2023-03-26 23:23:37 +0900

    230326 영어가 싫어요

---
+++ javascript/programmers/level0/영어가 싫어요.js
@@ -0,0 +1,22 @@
+function solution(numbers) {
+  let numStr = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  numStr.forEach((str, idx) => {
+    numbers = numbers.replaceAll(str, idx);
+  });
+  return Number(numbers);
+}
+
+console.log(solution("onetwothreefourfivesixseveneightnine"));
+console.log(solution("onefourzerosixseven"));
