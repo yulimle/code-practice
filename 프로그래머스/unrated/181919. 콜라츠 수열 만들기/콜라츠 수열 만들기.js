32bb0dc97d9c9597af932980d59d4f5b584e9a9c
Yulim Lee <81607335+yulimle@users.noreply.github.com>
2023-05-09 00:12:05 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.18 ms, Memory: 33.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  let answer = [n];
+  while (n !== 1) {
+    if (n % 2 === 0) {
+      n = n / 2;
+      answer.push(n);
+    } else {
+      n = 3 * n + 1;
+      answer.push(n);
+    }
+  }
+  return answer;
+}
