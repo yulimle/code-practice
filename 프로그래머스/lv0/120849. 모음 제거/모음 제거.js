11776b80760d5e674b6176e02ec2575f119b0d9b
Yulim Lee <81607335+yulimle@users.noreply.github.com>
2023-06-14 22:59:42 +0900

    [level 0] Title: 모음 제거, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+    my_string = my_string.replaceAll("a", "");
+   my_string = my_string.replaceAll("e", "");
+    my_string = my_string.replaceAll("i", "");
+    my_string = my_string.replaceAll("o", "");
+    my_string = my_string.replaceAll("u", "");
+  return my_string;
+}
