835c458aca9217e6cc19f001a2e501f2ccedc6d7
Yulim Lee <81607335+yulimle@users.noreply.github.com>
2024-05-11 18:30:16 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -0,0 +1,10 @@
+function solution(str_list) {
+    for(let i =0; i< str_list.length; i+=1){
+        if(str_list[i] === "l"){
+            return str_list.slice(0,i)
+        }else if(str_list[i] === "r"){
+            return str_list.slice(i+1, str_list.length)
+        }
+    }
+    return []
+}
